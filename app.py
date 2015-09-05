b6fbd4c1b5df1d682e0abd74459205e6a510bc40
Edward Kim <eddotman@gmail.com>
2015-09-05 12:12:48 -0400

    port

--- app.py
+++ app.py
@@ -9,4 +9,5 @@ def app_request_form(form):
   return request_form(form)
 
 if __name__ == '__main__':
-  app.run(os.environ.get('PORT', 5000))
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
