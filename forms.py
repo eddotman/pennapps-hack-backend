e222a3c46471750390b09c58647ea58679ed9eb7
Edward Kim <eddotman@gmail.com>
2015-09-05 13:12:57 -0400

    updated input fetching

--- forms.py
+++ forms.py
@@ -21,16 +21,28 @@ def request_form(form):
   }
 
   f_inputs = []
+  f_types = []
 
   for line in txtform:
     u_line = unicode(str(line).decode('latin_1').replace(u"\u2018", "").replace(u"\u2019", "").replace(u"\u201c","").replace(u"\u201d", "").replace(u"\u2014", "-"))
 
     if search('^([a-zA-Z]|\d)\.', u_line) is not None:
+      if search('(\$|\%|[aA]dd|[sS]ubtract|[mM]ultiply|[iI]ncome|[sS]alary|[wW]ages|[dD]ivid(e|end)|[iI]nterest|less|more|equal)', u_line) is not None:
+        f_types.append('currency')
+      elif search('\[*\]', u_line) is not None:
+        f_types.append('checkbox')
+      else:
+        f_types.append('text')
+      
+      if search('\:', u_line) is not None:
+        u_line = u_line[:u_line.index(":")+1]
+      
       f_inputs.append(u_line)
+
   
-  for f_input in f_inputs:
+  for f_input, f_type in zip(f_inputs, f_types):
     new_input = {
-      "type": "text",
+      "type": f_type,
       "name": f_input,
       "value": None,
       "coordinates": [None, None]
