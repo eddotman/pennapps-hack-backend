fec5350392ac3f46c930c36ca5e26df512570414
Edward Kim <eddotman@gmail.com>
2015-09-05 13:41:16 -0400

    added API

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
 from forms import (request_form)
 import os
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/<form>')
-def app_request_form(form):
-  return request_form(form)
+class Form(Resource):
+  def get(self, form):
+    return request_form(form)
+
+api.add_resource(Form, '/<form>')
 
 if __name__ == '__main__':
   port = int(os.environ.get("PORT", 5000))
