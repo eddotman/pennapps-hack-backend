39f1ac067a0af01d40785956c24c66b5665258ce
Edward Kim <eddotman@gmail.com>
2015-09-06 06:29:09 -0400

    a

--- forms.py
+++ forms.py
@@ -79,12 +79,16 @@ def translate_text(text, lang):
   return loads(r.text)['text']
 
 def save_form_json(form, data):
-  # if form == 'fw10':
-  #   fill_w10(form, data)
-  # else:
-  #   with open('jsons/' + str(form) + '.json', 'wb') as f:
-  #     f.write(dumps(data, indent=2, sort_keys=True))
-  fill_w10(form, data)
+  print "FORM"
+  print form
+  print "DATA"
+  print data
+  if form == 'fw10':
+    fill_w10(form, data)
+  else:
+    with open('jsons/' + str(form) + '.json', 'wb') as f:
+      f.write(dumps(data, indent=2, sort_keys=True))
+  
   return True
 
 def fill_w10(form, data):
