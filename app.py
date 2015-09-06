315668766a1822aeaeba4bc2085b1411086e073f
Edward Kim <eddotman@gmail.com>
2015-09-06 00:27:37 -0400

    post

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ class Form(Resource):
   def put(self, form):
     form_data = request.form
     save_form_json(form, form_data)
-    return 'Success!'
+    return 'SUCCESS:' + form_data
 
 api.add_resource(Form, '/<form>', '/<form>/<lang>')
 
