27e5df7c1ea33e2aee71a21ff3f2b9348883fb22
Edward Kim <eddotman@gmail.com>
2015-09-06 05:47:01 -0400

    pasd

--- forms.py
+++ forms.py
@@ -164,7 +164,7 @@ def populate_fields(pdfname, inputs):
 
           if value in fieldnamealt or fieldnamealt in value:
             fields.append((reference[fieldnamealt], key))
-            break
+            pass
           
 
   print "FIELDS"
